7d4088cb0a92a41b35333c7becd6807fbdec4a0e
Jacob Thomas <jacgretho@gmail.com>
2023-01-03 15:26:26 -0600

    initial commit

---
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,12 @@
+using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+    }
+}
