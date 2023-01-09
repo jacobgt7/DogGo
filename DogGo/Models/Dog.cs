e27d9f31756d67881dc1c86a84face1f24a5dc0a
Jacob Thomas <jacgretho@gmail.com>
2023-01-09 14:07:19 -0600

    added validation attributes to models

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DogGo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(35)]
         public string Name { get; set; }
+
         public int OwnerId { get; set; }
         public Owner Owner { get; set; }
+
+        [Required]
+        [MaxLength(35)]
         public string Breed { get; set; }
         public string Notes { get; set; }
         public string ImageUrl { get; set; }
